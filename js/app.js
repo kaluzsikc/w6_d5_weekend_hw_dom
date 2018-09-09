30471aeeb5cbb8c2273602a8c16699fd6ec3213f
kaluzsikc <kaluzsikc@gmail.com>
2018-09-09 17:46:06 +0100

    Add js delete all

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('js loaded');
+
   const newSearchForm = document.querySelector('#new-search-form');
   newSearchForm.addEventListener('submit', handleNewSearchFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
 })
 
-const handleNewSearchFormSubmit = function(event) {
+const handleNewSearchFormSubmit = function() {
   event.preventDefault();
   // console.log('events', this);
   const resultFlightClass = document.createElement('p');
-  resultFlightClass.textContent = `You selected ${this.classSelection.value} class`;
+  // resultFlightClass.textContent = `You selected ${this.classSelection.value} class`;
+  resultFlightClass.textContent = this.classSelection.value;
   const resultFlightType = document.createElement('p');
   resultFlightType.textContent = this.flightType.value;
   const resultDestinationFrom = document.createElement('p');
@@ -34,14 +39,28 @@ const handleNewSearchFormSubmit = function(event) {
   resultEmail.textContent = this.email.value;
 
   const results = document.createElement('div');
-  results.append(resultFlightClass, resultFlightType, resultDestinationFrom, resultDestinationTo, resultDepartureDate, resultReturnDate);
+  results.append(resultFlightClass, resultFlightType, resultDestinationFrom, resultDestinationTo, resultDepartureDate, resultReturnDate, resulttitle,resultFirstName, resultLastName, resultTelephone, resultEmail);
   // results.append(resultPriceRange);
 
-  const resultCustomerDetail = document.createElement('div');
-  results.append(resulttitle,resultFirstName, resultLastName, resultTelephone,resultEmail);
+  // const resultCustomerDetail = document.createElement('div');
+  // results.append(resulttitle,resultFirstName, resultLastName, resultTelephone, resultEmail);
+
+  results.classList.add('result-box');
 
   const result = document.querySelector('#result-list');
-  result.append(results, resultCustomerDetail);
-  // result.append(resultCustomerDetail);
+  result.append(results);
+
   this.reset();
+}
+
+// const handleDeleteAllClick = function (event) {
+//   const resultsList = document.querySelector('#result-list');
+//   resultsList.innerHTML = '';
+// }
+
+const handleDeleteAllClick = function () {
+  const listToDelete = document.querySelector('#result-list');
+  while (listToDelete.firstChild) {
+    listToDelete.removeChild(listToDelete.firstChild);
+  }
 }
